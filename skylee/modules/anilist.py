8c76165591589d3826a6e226b065bccba4d729b8
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-07-25 20:40:24 +0530

    Update anilist.py

--- skylee/modules/anilist.py
+++ skylee/modules/anilist.py
@@ -378,7 +378,7 @@ def manga(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
 
-
+@client.on_message(filters.command("nhentai"))
 async def nhentai(_, message):
     if len(message.command) < 2:
         await message.delete()
@@ -445,14 +445,12 @@ AIRING_HANDLER = CommandHandler("airing", airing)
 ANIME_HANDLER = CommandHandler("anime", anime)
 CHARACTER_HANDLER = CommandHandler("character", character)
 MANGA_HANDLER = CommandHandler("manga", manga)
-NHENTAI_HANDLER = nhentai, events.NewMessage(pattern="^[!/]nhentai$")
 
 dispatcher.add_handler(AIRING_HANDLER)
 dispatcher.add_handler(ANIME_HANDLER)
 dispatcher.add_handler(MANGA_HANDLER)
 dispatcher.add_handler(CHARACTER_HANDLER)
 
-client.add_event_handler(*NHENTAI_HANDLER)
 
 __help__ = """
 Get information about anime, manga or characters from [AniList](anilist.co) and [MyAnimeList](myanimelist.net).
