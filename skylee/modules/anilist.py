deb774246ca11e19096e4c33232603c427a67f37
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-07-25 21:01:04 +0530

    Update anilist.py

--- skylee/modules/anilist.py
+++ skylee/modules/anilist.py
@@ -378,80 +378,19 @@ def manga(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
 
-async def nhentai(_, message):
-    if len(message.command) < 2:
-        await message.delete()
-        return
-    query = message.text.split(None, 1)[1]
-    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(
-        query)
-    await message.reply_text(
-        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Read Here",
-                        url=post_url
-                    )
-                ]
-            ]
-        )
-    )
 
-def nhentai_data(noombers):
-    url = f"https://nhentai.net/api/gallery/{noombers}"
-    res = requests.get(url).json()
-    pages = res["images"]["pages"]
-    info = res["tags"]
-    title = res["title"]["english"]
-    links = []
-    tags = ""
-    artist = ''
-    total_pages = res['num_pages']
-    extensions = {
-        'j': 'jpg',
-        'p': 'png',
-        'g': 'gif'
-    }
-    for i, x in enumerate(pages):
-        media_id = res["media_id"]
-        temp = x['t']
-        file = f"{i+1}.{extensions[temp]}"
-        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
-        links.append(link)
-
-    for i in info:
-        if i["type"] == "tag":
-            tag = i['name']
-            tag = tag.split(" ")
-            tag = "_".join(tag)
-            tags += f"#{tag} "
-        if i["type"] == "artist":
-            artist = f"{i['name']} "
-
-    post_content = "".join(f"<img src={link}><br>" for link in links)
-
-    post = telegraph.create_page(
-        f"{title}",
-        html_content=post_content,
-        author_name="@Chizurumanagementbot",
-        author_url="https://t.me/Chizurumanagementbot"
-    )
-    return title, tags, artist, total_pages, post['url'], links[0]
 
 AIRING_HANDLER = CommandHandler("airing", airing)
 ANIME_HANDLER = CommandHandler("anime", anime)
 CHARACTER_HANDLER = CommandHandler("character", character)
 MANGA_HANDLER = CommandHandler("manga", manga)
-NHENTAI_HANDLER = nhentai, events.NewMessage(pattern="^[!/]nhentai$")
+
 
 dispatcher.add_handler(AIRING_HANDLER)
 dispatcher.add_handler(ANIME_HANDLER)
 dispatcher.add_handler(MANGA_HANDLER)
 dispatcher.add_handler(CHARACTER_HANDLER)
 
-client.add_event_handler(*NHENTAI_HANDLER)
 
 
 __help__ = """
