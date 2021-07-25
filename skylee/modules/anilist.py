5aacd1131f8bebcb227e99e633a6f3a62bf1453f
minatouzuki <minatouzuki.me@gmail.com>
2021-07-25 18:27:14 +0530

    rukabot: add nhentai support
    taken from: https://github.com/minatouzuki/mizuharaRobotx/blob/Mizuhara/Mizuhararobot/modules/anime.py

--- skylee/modules/anilist.py
+++ skylee/modules/anilist.py
@@ -26,6 +26,7 @@ from skylee import dispatcher
 import requests
 import math
 import time
+import telegraph
 
 
 def shorten(description, info="anilist.co"):
@@ -373,13 +374,90 @@ def manga(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
 
+@run_async
+def nhentai_data(noombers):
+    url = f"https://nhentai.net/api/gallery/{noombers}"
+    res = requests.get(url).json()
+    pages = res["images"]["pages"]
+    info = res["tags"]
+    title = res["title"]["english"]
+    links = []
+    tags = ""
+    artist = ''
+    total_pages = res['num_pages']
+    extensions = {
+        'j': 'jpg',
+        'p': 'png',
+        'g': 'gif'
+    }
+
+def nhentai(update: Update, context: CallbackContext):
+    message = update.effective_message
+    search = message.text.split(" ", 1)
+    if len(search) == 1:
+        message.reply_text("Format : /nhentai < ID >")
+        return
+    query = message.text.split(None, 1)[1]
+    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(
+        query)
+message.reply_text(
+        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Read Here",
+                        url=post_url
+                    )
+                ]
+            ]
+        )
+    )
+
+for i, x in enumerate(pages):
+        media_id = res["media_id"]
+        temp = x['t']
+        file = f"{i+1}.{extensions[temp]}"
+        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
+        links.append(link)
+
+for i in info:
+        if i["type"] == "tag":
+            tag = i['name']
+            tag = tag.split(" ")
+            tag = "_".join(tag)
+            tags += f"#{tag} "
+        if i["type"] == "artist":
+            artist = f"{i['name']} "
+
+post_content = "".join(f"<img src={link}><br>" for link in links)
+post = telegraph.create_page(
+        f"{title}",
+        html_content=post_content,
+        author_name="@missruka_bot",
+        author_url="https://t.me/missruka_bot"
+    )
+return title, tags, artist, total_pages, post['url'], links[0]
 
 AIRING_HANDLER = CommandHandler("airing", airing)
 ANIME_HANDLER = CommandHandler("anime", anime)
 CHARACTER_HANDLER = CommandHandler("character", character)
 MANGA_HANDLER = CommandHandler("manga", manga)
+NHENTAI_HANDLER = CommandHandler("nhentai", nhentai)
 
 dispatcher.add_handler(AIRING_HANDLER)
 dispatcher.add_handler(ANIME_HANDLER)
 dispatcher.add_handler(MANGA_HANDLER)
+dispatcher.add_handler(NHENTAI_HANDLER)
 dispatcher.add_handler(CHARACTER_HANDLER)
+
+__help__ = """
+Get information about anime, manga or characters from [AniList](anilist.co) and [MyAnimeList](myanimelist.net).
+*Available commands:*
+ ~ `/anime` <anime>: returns information about the anime from Anilist.
+ ~ `/character` <character>: returns information about the character from Anilist.
+ ~ `/manga` <manga>: returns information about the manga from Anilist.
+ ~ `/airing` <anime>: returns anime airing info.
+ ~ `/nhentai` <ID>: returns information from nhentai in telegraph page.
+ """
+__mod_name__ = "Anime"
